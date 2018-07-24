ed153d036926365ca6fd9ba52893d029ef95d8e6
John Humble <johnhhumble@gmail.com>
2018-07-24 02:03:11 -0600

    minor edits for debuging

--- examples/competition.cc
+++ examples/competition.cc
@@ -17,9 +17,9 @@ int main(int argc, char* argv[]) {
     JohnBot bot;
     
     coordinator.SetParticipants({
-        CreateParticipant(Race::Terran, &boi),
+       CreateParticipant(Race::Terran, &boi),
         CreateParticipant(Race::Zerg, &bot)
-       // CreateComputer(Race::Random, Difficulty::Medium)
+       // CreateComputer(Race::Random, Difficulty::VeryHard)
         });
 
     coordinator.LaunchStarcraft();
